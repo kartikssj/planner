68245bd014361d2c57b70f9ddb42ae182a6a1dff
kartiku <kartiku@uber.com>
2018-03-31 14:47:19 +0530

    Added redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-console.log(`I'm a silly entry point`);
+import "./css/main.css";
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import store from "./js/store";
+import App from "./js/components/App";
 
-const arr = [1, 2, 3];
-const iAmJavascriptES6 = () => console.log(...arr);
-window.iAmJavascriptES6 = iAmJavascriptES6;
-
-import App from "./App";
-import style from "./main.css";
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
---
+++ src/js/actions/index.js
@@ -0,0 +1,2 @@
+
+export const addArticle = article => ({ type: "ADD_ARTICLE", payload: article });
